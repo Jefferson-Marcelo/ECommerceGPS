fe9981ec2026c17a243552116cd0c15c536e3168
Gabriel Souto <gabriel.souto@academico.ifpb.edu.br>
2023-05-23 13:43:16 -0300

    reafactor layout

--- Front/src/environments/environment.prod.ts
+++ Front/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+import firebase from 'firebase/compat';
+
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC-H3V3RNnHyuV8onWu6tsrCd4rrwkmgc0",
+    authDomain: "ecommercece-154f7.firebaseapp.com",
+    projectId: "ecommercece-154f7",
+    storageBucket: "ecommercece-154f7.appspot.com",
+    messagingSenderId: "709718332543",
+    appId: "1:709718332543:web:b3ea94835038ea69a51b4a",
+    measurementId: "G-147ZGTGZVV"
+  }
 };
